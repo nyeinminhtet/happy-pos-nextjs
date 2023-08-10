a61788917367469ba2d02f0281336aad1f886bf6
Two J <TwoJ@gmail.com>
2023-08-10 14:00:47 +0630

    test

--- src/pages/order/activeCart/[id].tsx
+++ src/pages/order/activeCart/[id].tsx
@@ -9,66 +9,67 @@ import Image from "next/image";
 import { orderlines, orders } from "@prisma/client";
 import Loading from "@/Components/Loading";
 
-const ActiveOrder = () => {
-  const router = useRouter();
-  const { query } = router;
-  const orderId = query.id as string;
-  const dispatch = useAppDispatch();
-  const { orders, orderlines, menus, addons } = useAppSelector(appData);
-  const order = orders.find((item) => item.id === Number(orderId)) as orders;
+// const ActiveOrder = () => {
+//   const router = useRouter();
+//   const { query } = router;
+//   const orderId = query.id as string;
+//   const dispatch = useAppDispatch();
+//   const { orders, orderlines, menus, addons } = useAppSelector(appData);
+//   const order = orders.find((item) => item.id === Number(orderId)) as orders;
 
-  // const orderline = orderlines.filter((item) => item.order_id === order.id);
-  // console.log(orderlines);
+//   // const orderline = orderlines.filter((item) => item.order_id === order.id);
+//   // console.log(orderlines);
 
-  // const orderlineMenus = menus.find(
-  //   (menu) => menu.id === orderline?.menus_id
-  // )?.name;
+//   // const orderlineMenus = menus.find(
+//   //   (menu) => menu.id === orderline?.menus_id
+//   // )?.name;
 
-  // const orderlineAddons = addons.find(
-  //   (addon) => addon.id === orderline?.addons_id
-  // )?.name;
+//   // const orderlineAddons = addons.find(
+//   //   (addon) => addon.id === orderline?.addons_id
+//   // )?.name;
 
-  // useEffect(() => {
-  //   console.log(orderline);
-  // }, [orderline]);
+//   // useEffect(() => {
+//   //   console.log(orderline);
+//   // }, [orderline]);
 
-  useEffect(() => {
-    if (!order) {
-      router.push({ pathname: "/order", query });
-    }
-  }, [order, query, router]);
+//   useEffect(() => {
+//     if (!order) {
+//       router.push({ pathname: "/order", query });
+//     }
+//   }, [order, query, router]);
 
-  useEffect(() => {
-    dispatch(emptyCart());
-  }, []);
+//   useEffect(() => {
+//     dispatch(emptyCart());
+//   }, []);
 
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-      }}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          flexDirection: "column",
-          mt: -2,
-        }}
-      >
-        <Image src={cooking} alt="cooking" width={250} />
+//   return (
+//     <Box
+//       sx={{
+//         display: "flex",
+//         justifyContent: "center",
+//       }}
+//     >
+//       <Box
+//         sx={{
+//           display: "flex",
+//           alignItems: "center",
+//           justifyContent: "center",
+//           flexDirection: "column",
+//           mt: -2,
+//         }}
+//       >
+//         <Image src={cooking} alt="cooking" width={250} />
 
-        <Paper sx={{ width: 500 }}>
-          <Typography variant="h5">orderId: {order.id}</Typography>
-          <Typography variant="h5">Total-Price: {order.price}</Typography>
-          <Typography variant="h5">table: {order.table_id}</Typography>
-          {/* <Typography>{orderline?.status}</Typography> */}
-        </Paper>
-      </Box>
-    </Box>
-  );
-};
+//         <Paper sx={{ width: 500 }}>
+//           <Typography variant="h5">orderId: {order.id}</Typography>
+//           <Typography variant="h5">Total-Price: {order.price}</Typography>
+//           <Typography variant="h5">table: {order.table_id}</Typography>
+//           {/* <Typography>{orderline?.status}</Typography> */}
+//         </Paper>
+//       </Box>
+//     </Box>
+//   );
+// };
+const ActiveOrder = {};
 
 export default ActiveOrder;
